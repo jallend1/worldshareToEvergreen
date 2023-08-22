a1f6c650fb63166675e9b3936065b5604fe7f70b
Jason Allen <39307371+jallend1@users.noreply.github.com>
2023-08-22 13:39:51 -0700

    Achieves basic functionality

--- pasteToEvergreen.js
+++ pasteToEvergreen.js
@@ -1,20 +1,48 @@
 const titleInput = document.querySelector('#title-input');
-const callNumberInput = document.querySelector('#call-number-input');
+const callNumberInput = document.querySelector('#callnumber-input');
 const patronBarcodeInput = document.querySelector('#patron-barcode-input');
 const kclsBarcodeInput = document.querySelector('#item-barcode-input');
 
-const updateTitle = () => {
-    titleInput.value = 'Hello There';
-    const event = new Event('input', {
-        bubbles: true,
-        cancelable: true,
-    });
-    titleInput.dispatchEvent(event);
+let title;
+let requestNumber;
+let patronID;
+let addressString;
+
+const updateTitle = (title) => {
+  titleInput.value = 'ILL Title - ' + title;
+  const event = new Event('input', {
+    bubbles: true,
+    cancelable: true
+  });
+  titleInput.dispatchEvent(event);
+};
+
+const updateCallNumber = (requestNumber) => {
+  callNumberInput.value = 'IL' + requestNumber;
+  const event = new Event('input', {
+    bubbles: true,
+    cancelable: true
+  });
+  callNumberInput.dispatchEvent(event);
+};
+
+const updatePatronBarcode = (patronID) => {
+  patronBarcodeInput.value = patronID;
+  const event = new Event('input', {
+    bubbles: true,
+    cancelable: true
+  });
+  patronBarcodeInput.dispatchEvent(event);
 };
 
 const extractArrayFromClipboard = () => {
-    navigator.clipboard.readText().then((text) => {
-        console.log(text);
-        console.log(array);
-    });
-}
+  navigator.clipboard.readText().then((text) => {
+    console.log(text);
+    const array = JSON.parse(text);
+    updateTitle(array[2].title);
+    updateCallNumber(array[1].requestNumber);
+    updatePatronBarcode(array[3].patronID);
+  });
+};
+
+extractArrayFromClipboard();
